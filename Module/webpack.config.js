39315341d6c6b6e125db73823ec4224cfd4626c7
elimoreh <elimoreh@berkeley.edu>
2019-05-17 16:04:17 -0700

    fixes indent styling

--- Module/webpack.config.js
+++ Module/webpack.config.js
@@ -6,19 +6,19 @@ module.exports = {
   },
   module: {
     rules: [{
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          query: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
         },
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    },
     ],
   },
   resolve: {
