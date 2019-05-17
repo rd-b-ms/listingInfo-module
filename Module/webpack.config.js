d20559cb2ab6be4c72d5c41eb9760f62f9acbdb3
elimoreh <elimoreh@berkeley.edu>
2019-05-17 15:53:37 -0700

    configured test script

--- Module/webpack.config.js
+++ Module/webpack.config.js
@@ -1,29 +1,27 @@
 module.exports = {
-    entry: `${__dirname}/client/src/index.jsx`,
-    output: {
-      filename: 'bundle.js',
-      path: `${__dirname}/client/public`,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            query: {
-              presets: ['@babel/preset-env', '@babel/preset-react'],
-            },
+  entry: `${__dirname}/client/src/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: `${__dirname}/client/public`,
+  },
+  module: {
+    rules: [{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.js', '.jsx'],
-    },
-  };
-  
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
