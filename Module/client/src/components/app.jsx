b9228bb8d5ae201035f33a4d2af2538bcc79a3d4
elimoreh <elimoreh@berkeley.edu>
2019-05-18 19:49:17 -0700

    enables app to connect to server and recieve infromationfrom database

--- Module/client/src/components/app.jsx
+++ Module/client/src/components/app.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      // refers to the id number typed in the url
+      id: window.location.href.match(/id\s*=\s*(.*)/)[1] - 1,
+    };
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    axios.get('/listinginfo').then((response) => {
+      const { id } = this.state;
+      this.setState({ data: response.data[id] });
+    });
   }
 
   render() {
+    const { data } = this.state;
     return (
-      <div>
-        <p>App!</p>
+      <div className="container">
+        <p>{JSON.stringify(data)}</p>
       </div>
     );
   }
