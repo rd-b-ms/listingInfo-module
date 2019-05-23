61731a4e381861b051fa6ac2186011358818763b
elimoreh <elimoreh@berkeley.edu>
2019-05-22 19:48:04 -0700

    adds superhost svg medal to host that have the superhost attribute

--- Module/client/src/components/header.jsx
+++ Module/client/src/components/header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { medal } from '../styles/svg';
 import {
-  HeaderBox, HeaderText, Neighborhood, NeighborhoodLink, HostBox, HostImageBox, HostName,
+  HeaderBox, HeaderText, Neighborhood, NeighborhoodLink, HostBox,
+  HostImageBox, HostName, Medal, MedalBox,
 } from '../styles/style';
 
 const Header = (props) => {
-  const { hostName, header, neighborhood } = props;
-  const hostImage = `https://s3.amazonaws.com/fecfacedata/${hostName}.jpg`
+  const { hostName, header, neighborhood, superHost } = props;
+  const hostImage = `https://s3.amazonaws.com/fecfacedata/${hostName}.jpg`;
+  const hasMedal = (superHost) ? medal : 0;
 
   return (
     <div>
@@ -18,6 +21,9 @@ const Header = (props) => {
       </HeaderBox>
       <HostBox>
         <HostImageBox src={hostImage} />
+        <MedalBox>
+          <Medal>{hasMedal}</Medal>
+        </MedalBox>
         <HostName>{hostName}</HostName>
       </HostBox>
     </div>
@@ -28,6 +34,7 @@ Header.propTypes = {
   hostName: PropTypes.string.isRequired,
   header: PropTypes.string.isRequired,
   neighborhood: PropTypes.string.isRequired,
+  superHost: PropTypes.bool.isRequired,
 };
 
 export default Header;
